54d25ddaf686c808d11a6eaf28ec05784aa6ca98
Anderson Menezes <andersonmenezes.dev@gmail.com>
2023-11-26 10:45:59 -0300

    Print seller

--- src/main/java/org/andersonhsm/Main.java
+++ src/main/java/org/andersonhsm/Main.java
@@ -2,12 +2,15 @@ package org.andersonhsm;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Seller;
 
 public class Main {
     public static void main(String[] args) {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
-        sellerDao.findById(3);
+        Seller seller = sellerDao.findById(3);
+
+        System.out.println(seller);
     }
 }
